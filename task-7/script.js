3d4f64bdd0dc3a0df24b3e4180bcf2223057201d
dilinamr <dilinam91@gmail.com>
2023-03-22 23:15:10 +0530

    calculator with out eval function

--- task-7/script.js
+++ task-7/script.js
@@ -1,13 +1,47 @@
 function display(value) {
   document.getElementById("result").value += value;
 }
-function equal() {
-  var p = document.getElementById("result").value;
-  var q = eval(p);
-  document.getElementById("result").value = q;
+let input = "";
+let operation = "";
+
+function display(value) {
+  input += value;
+  document.getElementById("result").value = input;
 }
-function clearscreen() {
-  document.getElementById("result").value = "";
+function math(operator) {
+  operation = operator;
+  input += operator;
+  document.getElementById("result").value = input;
+}
+function calculate() {
+  let operands = input.split(operation);
+  let operand1 = parseFloat(operands[0]);
+  let operand2 = parseFloat(operands[1]);
+
+  let result;
+  switch (operation) {
+    case "+":
+      result = operand1 + operand2;
+      break;
+    case "-":
+      result = operand1 - operand2;
+      break;
+    case "*":
+      result = operand1 * operand2;
+      break;
+    case "/":
+      result = operand1 / operand2;
+      break;
+    case "%":
+      result = operand1 * (operand2 / 100);
+      break;
+  }
+  input = result.toString();
+  document.getElementById("result").value = input;
+}
+function clearResult() {
+  input = "";
+  document.getElementById("result").value = input;
 }
 function backspace() {
   document.getElementById("result").value = document
